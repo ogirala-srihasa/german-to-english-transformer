eccbbe14f2e39103242fbc2f0f9e8543c8017344
InfiniteOS6 <infiniteos456@gmail.com>
2026-05-15 19:16:23 +0530

    done

--- model.py
+++ model.py
@@ -475,7 +475,8 @@ class Transformer(nn.Module):
 
         ckpt = None
         if checkpoint_path == "auto":
-            GDRIVE_ID  = "1YIwEhGSNEWDPIAG1tUwSiWSfAqNWs5zt" 
+            # GDRIVE_ID  = "1YIwEhGSNEWDPIAG1tUwSiWSfAqNWs5zt" 
+            GDRIVE_ID  = "14xHNjW357C2h_g-ctUTpDGHSbfshIx75" 
             local_path = "best_checkpoint.pth"
             if not os.path.exists(local_path):
                 gdown.download(id=GDRIVE_ID, output=local_path, quiet=False)
