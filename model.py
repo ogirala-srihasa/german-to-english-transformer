30392d278441d39af57e375d458a7b378f2276b3
InfiniteOS6 <infiniteos456@gmail.com>
2026-05-17 00:24:33 +0530

    done

--- model.py
+++ model.py
@@ -476,7 +476,7 @@ class Transformer(nn.Module):
         ckpt = None
         if checkpoint_path == "auto":
             # GDRIVE_ID  = "1YIwEhGSNEWDPIAG1tUwSiWSfAqNWs5zt" 
-            GDRIVE_ID  = "12iKFi12TJe8Br5gmGC8kWHtXMGDpySGc"
+            GDRIVE_ID  = "1ddDYtGA7_Neahck0aY1Hk4XOSOT1Zl6Q"
             # GDRIVE_ID  = "1KnauGrqbg1a0duvmFmoBr9B22MR2cFWn" 
             local_path = "best_checkpoint.pth"
             if not os.path.exists(local_path):
